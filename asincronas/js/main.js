dbea2f9241bcd72600b342d5d6c9d5407af41f74
Ana12002 <94591141+Ana12002@users.noreply.github.com>
2021-12-01 23:32:31 -0600

    s

--- asincronas/js/main.js
+++ asincronas/js/main.js
@@ -4,7 +4,7 @@ let arrayRegistros = [];
 async function leeRegistros(){
     //var nombre= document.getElementById('nombre').value;
     //var ip = document.getElementById('ip').value;
-    const respuesta = await fetch("http://www.geoplugin.net/json.gp?ip=189.142.199.18");
+    const respuesta = await fetch("http://www.geoplugin.net/json.gp?ip=187.188.202.137");
     const registros = await respuesta.json();
     
 
@@ -20,12 +20,12 @@ async function muestraRegistros(){
         console.log("Error generado:" +e);
     }
     console.log(arrayRegistros);
-    RegistrosHTML.innerHTML = RegistrosHTML.innerHTML + "IP:"+ arrayRegistros.geoplugin_request +  "<br/> Ciudad: "+ arrayRegistros.geoplugin_city + "<br/> Pais: " + arrayRegistros.geoplugin_countryName;
+    //RegistrosHTML.innerHTML = RegistrosHTML.innerHTML + "IP:"+ arrayRegistros.geoplugin_request +  "<br/> Ciudad: "+ arrayRegistros.geoplugin_city + "<br/> Pais: " + arrayRegistros.geoplugin_countryName;
     
-   /*   arrayRegistros.forEach(function (item, index) {
-        RegistrosHTML.innerHTML = RegistrosHTML.innerHTML + "IP" + item.geoplugin_request + item.geoplugin_city + " " + item.geoplugin_countryName + "<br>";
+   arrayRegistros.forEach(function (item, index) {
+        RegistrosHTML.innerHTML = RegistrosHTML.innerHTML + "IP: " + item.geoplugin_request  +  "<br/> Ciudad: "+ item.geoplugin_city + " <br/> Pais: " + item.geoplugin_countryName + "<br>";
 
-    });*/
+    });
     
     
 }
