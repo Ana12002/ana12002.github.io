1787b067fe3695c4b9c2ecf0fcd4efe987622bc1
Ana12002 <94591141+Ana12002@users.noreply.github.com>
2021-12-02 00:55:34 -0600

    se

--- asincronas/js/main.js
+++ asincronas/js/main.js
@@ -4,7 +4,7 @@ let arrayRegistros = [];
 async function leeRegistros(){
     //var nombre= document.getElementById('nombre').value;
     //var ip = document.getElementById('ip').value;
-    const respuesta = await fetch("http://www.geoplugin.net/json.gp?ip=187.188.202.137");
+    const respuesta = await fetch("https://backendakcp.azurewebsites.net/cliente.php");
     const registros = await respuesta.json();
     
 
@@ -20,7 +20,7 @@ async function muestraRegistros(){
         console.log("Error generado:" +e);
     }
     console.log(arrayRegistros);
-    RegistrosHTML.innerHTML = RegistrosHTML.innerHTML + "IP:"+ arrayRegistros.geoplugin_request +  "<br/> Ciudad: "+ arrayRegistros.geoplugin_city + "<br/> Pais: " + arrayRegistros.geoplugin_countryName;
+    RegistrosHTML.innerHTML = RegistrosHTML.innerHTML + "IP:"+ arrayRegistros.Ip +  "<br/> Ciudad: "+ arrayRegistros.Ciudad + "<br/> Pais: " + arrayRegistros.Pais;
     
    /*arrayRegistros.forEach(function (item, index) {
         RegistrosHTML.innerHTML = RegistrosHTML.innerHTML + "IP: " + item.geoplugin_request  +  "<br/> Ciudad: "+ item.geoplugin_city + " <br/> Pais: " + item.geoplugin_countryName + "<br>";
